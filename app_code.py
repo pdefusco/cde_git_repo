e622f8ecc9c81d89eb452310997183f2bb569267
pdefusco <pauldefusco@gmail.com>
2025-04-03 17:34:30 -0700

    updated util path

--- app_code.py
+++ app_code.py
@@ -38,7 +38,7 @@
 #***************************************************************************/
 
 from pyspark.sql import SparkSession
-from app_util import util_function
+from function_repo.app_util import util_function
 
 def main():
     spark = SparkSession.builder \
